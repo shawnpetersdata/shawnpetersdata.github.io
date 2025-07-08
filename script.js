b9b0a354d9ae2907bebbc84fbc144ea426153bde
Shawn Peters <shawnpetersdata@gmail.com>
2025-07-08 20:38:01 -0230

    Fixing pop-ups

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
 const artworks = [
     { 
+        src: "images/flock.png", 
+        description: "", 
+        details: "945 x 945 pixels, Digital, 2024" 
+    },
+    { 
+        src: "images/landscape.png", 
+        description: "", 
+        details: "707 x 707 pixels, Digital, 2025" 
+    },
+    { 
+        src: "images/tau.png", 
+        description: "", 
+        details: "992 x 992 pixels, Digital, 2025" 
+    },
+    { 
+        src: "images/triangle.png", 
+        description: "", 
+        details: "926 x 926 pixels, Digital, 2025" 
+    },
+    { 
+        src: "images/living mosiac.png", 
+        description: "", 
+        details: "2340 x 2808, Digital, 2025" 
+    },
+    { 
         src: "images/klondyke.png", 
-        description: "A surreal take on modern landscapes, combining natural and digital elements.", 
-        details: "60 x 48 in, Digital, 2024" 
+        description: "", 
+        details: "14 x 11 inches, Digital on canvas, 2025" 
     },
     { 
-        src: "images/work2.jpg", 
-        description: "Exploring the connection between fluidity and structure through algorithmic patterns.", 
-        details: "72 x 60 in, Oil on Canvas, 2023" 
+        src: "images/passinglight.jpg", 
+        description: "", 
+        details: "17 x 11 inches, Digital on canvas, 2025" 
     },
     { 
-        src: "images/work3.jpg", 
-        description: "A study in generative randomness, creating unique compositions each time.", 
-        details: "48 x 36 in, Generative Art, 2022" 
+        src: "images/fragments.jpg", 
+        description: "", 
+        details: "20 x 10 inches, Digital on canvas, 2025" 
+    },
+    { 
+        src: "images/beyond.jpg", 
+        description: "", 
+        details: "8 x 10 inches, Digital on canvas, 2025" 
+    },
+    { 
+        src: "images/shagrock.png", 
+        description: "", 
+        details: "3600 x 3600 pixels, Digital, 2025" 
     }
+
 ];
 
 let currentIndex = 0;
